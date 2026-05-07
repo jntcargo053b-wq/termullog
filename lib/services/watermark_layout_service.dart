620eda97d3a3214d423c5387dd569503e4a2f0a2
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-05-07 10:36:15 +0700

    Implement watermark position management
    Added static position variable and methods to load and save watermark position using SharedPreferences.

--- lib/services/watermark_layout_service.dart
+++ lib/services/watermark_layout_service.dart
@@ -5,7 +5,22 @@
 
 import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+class WatermarkLayoutService {
+  static String position = 'bottom'; // ← ini yang hilang
+
+  static Future<void> load() async {
+    final prefs = await SharedPreferences.getInstance();
+    position = prefs.getString('watermark_position') ?? 'bottom';
+  }
+
+  static Future<void> save(String pos) async {
+    position = pos;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('watermark_position', pos);
+  }
+}
 class WatermarkLayoutService {
   WatermarkLayoutService._();
 

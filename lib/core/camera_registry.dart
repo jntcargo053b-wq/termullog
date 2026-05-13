92228775c7ec789d8380dee377ac844db7ef28c2
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-05-13 12:34:43 +0700

    Delete lib directory

--- lib/core/camera_registry.dart
+++
@@ -1,9 +0,0 @@
-import 'package:camera/camera.dart';
-
-class CameraRegistry {
-  static List<CameraDescription> cameras = [];
-  
-  static Future<void> initialize() async {
-    cameras = await availableCameras();
-  }
-}

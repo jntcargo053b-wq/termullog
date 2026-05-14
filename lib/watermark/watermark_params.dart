ff02f3fad0028513066200483fd4eabfdd4c7503
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-05-14 12:15:48 +0700

    Update watermark_params.dart

--- lib/watermark/watermark_params.dart
+++ lib/watermark/watermark_params.dart
@@ -33,10 +33,44 @@ class WatermarkParams {
     this.acc,
   });
 
-  // Getter untuk akses mudah
   Uint8List get imageBytes => transferable.materialize().asUint8List();
   Uint8List? get mapBytes => mapTransferable?.materialize().asUint8List();
 
-  Map<String, dynamic> toMap() { ... } // sama seperti sebelumnya
-  static WatermarkParams fromMap(Map<String, dynamic> map) { ... } // sama
+  Map<String, dynamic> toMap() {
+    return {
+      'transferable': transferable,
+      'mapTransferable': mapTransferable,
+      'timestamp': timestamp.toIso8601String(),
+      'address': address,
+      'weather': weather,
+      'layoutIndex': layoutIndex,
+      'showWeather': showWeather,
+      'showAccuracy': showAccuracy,
+      'watermarkPosition': watermarkPosition,
+      'showMiniMap': showMiniMap,
+      'lat': lat,
+      'lon': lon,
+      'acc': acc,
+    };
+  }
+
+  static WatermarkParams fromMap(Map<String, dynamic> map) {
+    final transferable = map['transferable'] as TransferableTypedData;
+    final mapTransferable = map['mapTransferable'] as TransferableTypedData?;
+    return WatermarkParams(
+      transferable: transferable,
+      mapTransferable: mapTransferable,
+      timestamp: DateTime.parse(map['timestamp'] as String),
+      address: map['address'] as String,
+      weather: map['weather'] as String,
+      layoutIndex: map['layoutIndex'] as int,
+      showWeather: map['showWeather'] as bool,
+      showAccuracy: map['showAccuracy'] as bool,
+      watermarkPosition: map['watermarkPosition'] as String,
+      showMiniMap: map['showMiniMap'] as bool,
+      lat: map['lat'] as double?,
+      lon: map['lon'] as double?,
+      acc: map['acc'] as double?,
+    );
+  }
 }

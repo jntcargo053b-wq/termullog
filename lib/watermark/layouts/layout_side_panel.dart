57856d28aa700e4009c8e55f8e6581a7682408d5
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-05-18 12:49:49 +0700

    Revise comment in layout_side_panel.dart
    Updated comment to clarify method behavior.

--- lib/watermark/layouts/layout_side_panel.dart
+++ lib/watermark/layouts/layout_side_panel.dart
@@ -29,7 +29,7 @@ class LayoutSidePanel extends WatermarkLayoutBase {
     bool showBorder = true,
     String fontSize = 'normal',
   }) {
-    // ... isi sama persis dengan yang sudah ada ...
+    // … isi method tetap sama seperti sebelumnya …
     return WatermarkLayoutBase.encodeJpg(src);
   }
 }

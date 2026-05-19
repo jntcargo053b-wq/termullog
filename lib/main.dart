7292c43c42dce357e4962455a069e7a1099e52d5
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-05-19 21:34:48 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,19 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'core/camera_registry.dart';
 import 'ui/app.dart';
 
+import 'watermark/watermark_utils.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Load fonts
+  await WatermarkFontManager.loadFonts();
+  
+  // Migrasi settings
+  await SettingsService.migrateOldSettings();
+  
+  runApp(const MyApp());
+}
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 

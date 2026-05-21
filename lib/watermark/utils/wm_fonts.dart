8d80f6467ab885618529602e10af396034c4a483
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-05-21 19:22:43 +0700

    Add WmFonts class with bitmap font definitions

---
+++ lib/watermark/utils/wm_fonts.dart
@@ -0,0 +1,8 @@
+import 'package:image/image.dart' as img;
+import 'package:image/image.dart';
+
+class WmFonts {
+  static final img.BitmapFont small = img.arial14;
+  static final img.BitmapFont medium = img.arial24;
+  static final img.BitmapFont large = img.arial48;
+}

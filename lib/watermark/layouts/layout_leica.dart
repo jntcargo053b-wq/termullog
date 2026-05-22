57460d9205f9f79adfc4f2a2c2daaf8d4f7e3a4b
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-05-22 11:35:24 +0700

    Update layout_leica.dart

--- lib/watermark/layouts/layout_leica.dart
+++ lib/watermark/layouts/layout_leica.dart
@@ -1,10 +1,6 @@
-// lib/watermark/layouts/layout_leica.dart
 import 'dart:typed_data';
 import 'package:intl/intl.dart';
 import 'package:image/image.dart' as img;
-import 'package:image/src/font/arial_12.dart';
-import 'package:image/src/font/arial_14.dart';
-import 'package:image/src/font/arial_24.dart';
 import 'watermark_layout_base.dart';
 import '../../core/constants.dart';
 
@@ -36,43 +32,32 @@ class LayoutLeica extends WatermarkLayoutBase {
     final int margin = 20;
     final int x = src.width - margin - 180;
     int y = src.height - margin - 80;
-    
-    // Perbaikan: fillCircle menggunakan positional parameters (versi 3.0.5)
-    // Parameter: fillCircle(image, x0, y0, radius, color)
-    img.fillCircle(
-      src,                          // image
-      src.width - margin - 12,      // x0
-      y + 12,                       // y0
-      6,                            // radius
-      kColorRed                     // color
-    );
-    
-    // Text
+
+    img.fillCircle(src, src.width - margin - 12, y + 12, 6, kColorRed);
+
     final String dateStr = DateFormat('yyyy-MM-dd').format(timestamp);
     final String timeStr = DateFormat('HH:mm:ss').format(timestamp);
-    
+
     _drawText(src, dateStr, x, y, 14, kColorWhite);
     y += 24;
     _drawText(src, timeStr, x, y, 14, kColorWhite);
     y += 24;
-    
+
     if (hasPosition && showCoordinates && lat != null && lon != null) {
       final String coordStr = '${lat.toStringAsFixed(4)}° ${lon.toStringAsFixed(4)}°';
       _drawText(src, coordStr, x, y, 11, kColorLightGrey);
       y += 20;
     }
-    
-    // Accuracy
+
     if (showAccuracy && acc != null) {
       final String accStr = '±${acc.toStringAsFixed(1)}m';
       _drawText(src, accStr, x, y, 11, getAccuracyColor(acc));
     }
-    
+
     return WatermarkLayoutBase.encodeJpg(src);
   }
-  
-  void _drawText(img.Image image, String text, int x, int y, int size, img.Color color) {
-    // Perbaikan: Menggunakan font yang sesuai dengan size
+
+  void _drawText(img.Image image, String text, int x, int y, int size, int color) {
     if (size <= 12) {
       img.drawString(image, img.arial12, x, y, text, color: color);
     } else if (size <= 14) {

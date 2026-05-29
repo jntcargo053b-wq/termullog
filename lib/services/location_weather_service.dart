9b9a09cd97aa3b9cf07f54a52ad6a18bf4e566ad
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-05-29 08:21:16 +0700

    Update location_weather_service.dart

--- lib/services/location_weather_service.dart
+++ lib/services/location_weather_service.dart
@@ -96,7 +96,7 @@ class LocationWeatherService {
   // Cache alamat dengan radius 5m (fix #1)
   static final Map<String, _AddressCacheEntry> _addressCache = {};
   static const int _addressCacheMaxSize = 100;
-  static const double _addressCacheRadiusMeters = 2.0;
+  static const double _addressCacheRadiusMeters = 1.5;
 
   // Cache weather TTL 10 menit
   static final Map<String, _WeatherCacheEntry> _weatherCache = {};

791658533ec3567a1e55cdc34616f17bdb02aab7
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-06-05 13:37:04 +0700

    Delete lib/watermark/utils/wm_fonts.dart

--- lib/watermark/utils/wm_fonts.dart
+++
@@ -1,8 +0,0 @@
-import 'package:image/image.dart' as img;
-import 'package:image/image.dart';
-
-class WmFonts {
-  static final img.BitmapFont small = img.arial14;
-  static final img.BitmapFont medium = img.arial24;
-  static final img.BitmapFont large = img.arial48;
-}

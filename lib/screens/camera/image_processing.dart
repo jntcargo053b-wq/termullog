11b78c40a89e4a81ba3ba2b0d4fdb1e755b00028
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-06-12 12:31:21 +0700

    Create image_processing.dart

---
+++ lib/screens/camera/image_processing.dart
@@ -0,0 +1,36 @@
+import 'dart:typed_data';
+import 'package:flutter/foundation.dart';
+import 'package:image/image.dart' as img;
+
+@immutable
+class ResizeParams {
+  final Uint8List rawBytes;
+  final int quality;
+  const ResizeParams(this.rawBytes, this.quality);
+}
+
+Future<Uint8List> resizeImageIsolate(ResizeParams params) async {
+  final originalImg = img.decodeImage(params.rawBytes);
+  if (originalImg == null) return params.rawBytes;
+  const int targetWidth = 1920;
+  if (originalImg.width <= targetWidth) {
+    return Uint8List.fromList(img.encodeJpg(originalImg, quality: params.quality));
+  }
+  final ratio = originalImg.height / originalImg.width;
+  final h = (targetWidth * ratio).round();
+  final resized = img.copyResize(originalImg, width: targetWidth, height: h);
+  return Uint8List.fromList(img.encodeJpg(resized, quality: params.quality));
+}
+
+Future<Uint8List> resizeImageSync(Uint8List rawBytes, int quality) async {
+  final originalImg = img.decodeImage(rawBytes);
+  if (originalImg == null) return rawBytes;
+  const int targetWidth = 1920;
+  if (originalImg.width <= targetWidth) {
+    return Uint8List.fromList(img.encodeJpg(originalImg, quality: quality));
+  }
+  final ratio = originalImg.height / originalImg.width;
+  final h = (targetWidth * ratio).round();
+  final resized = img.copyResize(originalImg, width: targetWidth, height: h);
+  return Uint8List.fromList(img.encodeJpg(resized, quality: quality));
+}

e4f1e57d75b9a80d1b3d294aae0667d1944fe972
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-06-18 14:17:13 +0700

    Update settings_screen.dart

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -6,7 +6,6 @@ import 'dart:typed_data';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as path;
 import '../core/constants.dart';
 import '../services/settings_cache.dart';
 
